a227cd02d6b17673107feb72236c852f6da32b35
Franzwagner Ternus <franzwagner.str@gmail.com>
2020-05-29 12:46:23 -0300

    Update Brew install script to check if macOS or Linux

--- brew/installBrew.sh
+++ brew/installBrew.sh
@@ -4,10 +4,17 @@ GREEN='\033[1;32m'
 BACKGROUND='\e[0;39;41m'
 NC='\033[0m' # No Color
 
-# Install Xcode
-echo -ne "\n${GREEN} Installing Xcode${NC}\n\n"
-xcode-select --install
-
-# Install Brew
-echo -ne "\n\n${GREEN} Installing Brew${NC}\n\n"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     # Install Brew
+                echo -ne "\n\n${GREEN} Installing Brew${NC}\n\n"
+                /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+                exit 0;;
+    Darwin*)    # Install Xcode
+                echo -ne "\n${GREEN} Installing Xcode${NC}\n\n"
+                xcode-select --install
+                # Install Brew
+                echo -ne "\n\n${GREEN} Installing Brew${NC}\n\n"
+                /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+                exit 0;;
+esac
